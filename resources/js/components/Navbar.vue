b8941dd171bcedd8b041a9064d7d88890da4de80
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2024-11-17 18:59:14 -0300

    create navbar component

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-app-bar :elevation="2">
+        <v-app-bar-title>O Patusco</v-app-bar-title>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: "Navbar",
+};
+</script>
