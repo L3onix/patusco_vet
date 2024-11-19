fa5148173540b93e9d1d0c9e0ef74b15f6318167
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2024-11-19 03:44:40 -0300

    create appointments routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\AppointmentController;
+use App\Http\Controllers\PetController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/appointments', [AppointmentController::class, 'index']);
+Route::get('/appointments/{id}', [AppointmentController::class, 'show']);
+Route::put('/appointments/{id}', [AppointmentController::class, 'update']);
+Route::delete('/appointments/{id}', [AppointmentController::class, 'destroy']);
