4c25c12e743ce5678efe6c38c6c15b5564b5c8fe
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2024-11-19 03:44:07 -0300

    create methods to list animal types

--- app/Enums/PetAnimalTypes.php
+++ app/Enums/PetAnimalTypes.php
@@ -8,4 +8,14 @@ enum PetAnimalTypes: string
     case CAT = 'gato';
     case BIRD = 'pássaro';
     case OTHER = 'outro';
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+
+    public static function labels(): array
+    {
+        return array_map(fn ($case) => ucfirst($case->value), self::cases());
+    }
 }
