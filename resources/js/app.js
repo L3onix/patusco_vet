43550c1349289a9b25a11292724b2c41ead54254
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2024-11-19 03:42:19 -0300

    setup vuetify labs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,24 @@
 import './bootstrap';
 import { createApp } from 'vue';
 
-//vuetify
+// vuetify
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import * as labsComponents from 'vuetify/labs/components';
 
 import App from './App.vue';
 
 const vuetify = createVuetify({
-    components,
+    components: {
+        ...components,
+        ...labsComponents,
+    },
+    date: {
+        locale: 'en',
+    },
     directives,
-})
+});
 
 createApp(App).use(vuetify).mount('#app');
